0608788e32937f57698b9bd1fa3a19c03a9d7d34
Matthias Frey <info@frey-it.com>
2021-10-22 10:40:22 +0200

    project: fudge dependency only for testing

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ setup(
         '': ['README.rst']
     },
     install_requires=[
-        'fudge>=1.0.3',
         'requests>=0.14.0',
         'simplejson>=2.0',
     ],
     tests_require=[
         'nose>=1.1.2',
+        'fudge>=1.0.3',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
